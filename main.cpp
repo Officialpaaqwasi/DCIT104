94c954a5581b3ae65d3a81119aca456eafa25c49
Isaac Klutse <102985455+10970601@users.noreply.github.com>
2022-08-08 16:59:04 +0000

    Add files via upload
    Using c++ to compute the average of  prime numbers of a given number

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+ //KLUTSE ISAAC   10970601 //
+using namespace std;
+
+int main()
+{
+	int l, average = 0, sum = 0, count_prime = 0 ;
+	cout << " Enter your value: ";
+	cin >> l;
+
+
+	for(int i =2; i <= l; i ++)
+  {
+		int n;
+		for(n=2; n<i; n++){
+			if(i%n == 0){
+				break;
+				}
+		}
+		if(n == i){
+			sum = sum + i;
+			count_prime ++;
+				}
+  }
+
+	cout << "The number of prime numbers available is: " << count_prime << endl << endl;
+	cout << "The sum of the numbers is: " << sum << endl << endl;
+
+	average = sum/count_prime;
+	cout << "The average is: " << average << endl;
+
+return 0;
+}
